a7ddc8fb34246b8bf75256bcad1fe58c51495944
Lukman <lukman.fyrtio@gmail.com>
2024-02-24 18:33:25 +0700

    Add invoice controller

--- src/main/java/com/id/sales/service/dto/AddRequestTarget.java
+++ src/main/java/com/id/sales/service/dto/AddRequestTarget.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddRequestTarget {
-	
+
 	private String bup;
 	private String tahun;
 	private List<MonthClass> existing;
