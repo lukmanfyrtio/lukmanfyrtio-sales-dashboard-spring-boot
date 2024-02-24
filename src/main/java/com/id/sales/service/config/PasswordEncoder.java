dffc45d75698691d968619d4315015ac94699907
Lukman <lukman.fyrtio@gmail.com>
2024-02-24 18:02:14 +0700

    Add cors config and password encoder

---
+++ src/main/java/com/id/sales/service/config/PasswordEncoder.java
@@ -0,0 +1,14 @@
+package com.id.sales.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoder {
+
+	@Bean
+	protected BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
