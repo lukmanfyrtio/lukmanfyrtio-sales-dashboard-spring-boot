1c65c6bc7d7987b40112509b181061e90314dcec
Lukman <lukman.fyrtio@gmail.com>
2024-02-24 17:59:59 +0700

    Initialize project spring boot

---
+++ src/main/java/com/id/sales/service/SalesPerformanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.id.sales.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesPerformanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesPerformanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/id/sales/service/SalesPerformanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.id.sales.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPerformanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
